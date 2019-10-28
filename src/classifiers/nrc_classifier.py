3df25a0984b8b79108b0a91cb0263614bfe4f866
Maysam Mokarian <mamokari@microsoft.com>
2019-10-27 21:57:11 -0400

    Adds generate_personality_traint_data

--- src/classifiers/nrc_classifier.py
+++ src/classifiers/nrc_classifier.py
@@ -1,4 +1,18 @@
-
+from src.util import Utils
+import pandas as pd
 
 class NRCClassifier:
-    pass
+    @staticmethod
+    def generate_personality_trait_data(personality_trait):
+        util = Utils()
+        profile_df = util.read_data_to_dataframe("../../data/Train/Profile/Profile.csv")
+        nrc_df = util.read_data_to_dataframe("../../data/Train/Text/nrc.csv")
+        nrc_df.rename(columns={'userId': 'userid'}, inplace=True)
+        merged_df = pd.merge(nrc_df, profile_df, on='userid')
+        return merged_df.filter(['positive', 'negative', 'anger', 'anticipation', 'disgust',
+       'fear', 'joy', 'sadness', 'surprise', 'trust', personality_trait], axis=1)
+
+
+if __name__ == "__main__":
+    df = NRCClassifier().generate_gender_data("ope")
+    gooz =""
